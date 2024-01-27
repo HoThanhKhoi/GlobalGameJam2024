96ce60c75170f4fa01a68a460ca2d516f3916376
HoThanhKhoi <th.khoi2003@gmail.com>
2024-01-27 16:05:03 +0700

    [Add] Add Explosiopn, [Update] Change shooting to Raycast, erase bullet

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance{get;private set;}
+
+    [SerializeField] public LayerMask playerLayer;
+    [SerializeField] public LayerMask enemyLayer;
+
+    private void Awake() {
+        if(Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
